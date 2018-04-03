daf153e957dc570de8adfec1d79b9c36cf16cbcb
Yauheni Shcharbuk <Yauheni_Shcharbuk@epam.com>
2018-04-03 19:11:20 +0300

    Menu

--- app/src/main/java/com/example/stanislau_bushuk/foodhealth/MainActivity.java
+++ app/src/main/java/com/example/stanislau_bushuk/foodhealth/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.stanislau_bushuk.foodhealth;
 
+import android.support.annotation.BinderThread;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnItemSelected;
+import butterknife.OnTouch;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        //Menu-------------------
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.search:
+                        //selectedFragment = ItemOneFragment.newInstance();
+                        break;
+                    case R.id.search_deep:
+                        //selectedFragment = ItemTwoFragment.newInstance();
+                        break;
+                    case R.id.featured:
+                        //selectedFragment = ItemThreeFragment.newInstance();
+                        break;
+                }
+/*                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, selectedFragment);
+                transaction.commit();*/
+                return true;
+            }
+        });
+
+        //-------------------
     }
 }
--- app/src/main/java/com/example/stanislau_bushuk/foodhealth/View/ViewSearch.java
+++ app/src/main/java/com/example/stanislau_bushuk/foodhealth/View/ViewSearch.java
@@ -2,6 +2,6 @@ package com.example.stanislau_bushuk.foodhealth.View;
 
 import com.arellomobile.mvp.InjectViewState;
 
-@InjectViewState
+
 public interface ViewSearch {
 }
