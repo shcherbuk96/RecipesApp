e3ec35de1027f168cb06689b8fc996df56432fbf
logoped583st <logoped583st@mail.ru>
2018-04-06 23:01:52 +0300

    more trash with folders

---
+++ app/src/main/java/com/example/stanislau_bushuk/foodhealth/model/pojo/lists/HealhLabels.java
@@ -0,0 +1,7 @@
+package com.example.stanislau_bushuk.foodhealth.model.pojo.lists;
+
+import io.realm.RealmObject;
+
+public class HealhLabels extends RealmObject {
+    private String string;
+}
---
+++ app/src/main/java/com/example/stanislau_bushuk/foodhealth/model/pojo/lists/Ingridients.java
@@ -0,0 +1,7 @@
+package com.example.stanislau_bushuk.foodhealth.model.pojo.lists;
+
+import io.realm.RealmObject;
+
+public class Ingridients extends RealmObject {
+    private String string;
+}
