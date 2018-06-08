6642358d436cce855aafbe388f61a594b4f38f9d
Yauheni_Shcharbuk <yauheni_shcharbuk@epam.com>
2018-06-08 12:23:02 +0300

    fix

--- app/src/main/java/com/example/stanislau_bushuk/foodhealth/model/pojo/OwnRecipe.java
+++ app/src/main/java/com/example/stanislau_bushuk/foodhealth/model/pojo/OwnRecipe.java
@@ -1,10 +1,8 @@
 package com.example.stanislau_bushuk.foodhealth.model.pojo;
 
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.GenericTypeIndicator;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class OwnRecipe {
 
@@ -12,6 +10,27 @@ public class OwnRecipe {
     private String recipeInstruction;
     private String recipePhoto;
     private ArrayList<String> recipeIngridients;
+
+    public OwnRecipe(final DataSnapshot ds) {
+        this.recipeName = ds.child("recipeName").getValue(String.class);
+        this.recipePhoto = ds.child("recipePhoto").getValue(String.class);
+
+        this.recipeIngridients = new ArrayList<>();
+
+
+        recipeIngridients.addAll(ds.child("recipeIngridients").getValue(ArrayList.class));
+        this.recipeInstruction = ds.child("recipeInstruction").getValue(String.class);
+    }
+
+    public OwnRecipe(final String recipeName, final String recipeInstruction, final String recipePhoto, final ArrayList<String> recipeIngridients) {
+        this.recipeName = recipeName;
+        this.recipeInstruction = recipeInstruction;
+        this.recipePhoto = recipePhoto;
+        this.recipeIngridients = recipeIngridients;
+    }
+
+    public OwnRecipe() {
+    }
 
     public String getRecipeName() {
         return recipeName;
@@ -44,29 +63,6 @@ public class OwnRecipe {
     public void setRecipeIngridients(final ArrayList<String> recipeIngridients) {
         this.recipeIngridients = recipeIngridients;
     }
-
-    public OwnRecipe(final DataSnapshot ds) {
-        this.recipeName = ds.child("recipeName").getValue(String.class);
-        this.recipePhoto = ds.child("recipePhoto").getValue(String.class);
-
-        this.recipeIngridients = new ArrayList<>();
-
-
-        recipeIngridients.addAll(ds.child("recipeIngridients").getValue(ArrayList.class));
-        this.recipeInstruction = ds.child("recipeInstruction").getValue(String.class);
-    }
-
-    public OwnRecipe(final String recipeName, final String recipeInstruction, final String recipePhoto, final ArrayList<String> recipeIngridients) {
-        this.recipeName = recipeName;
-        this.recipeInstruction = recipeInstruction;
-        this.recipePhoto = recipePhoto;
-        this.recipeIngridients = recipeIngridients;
-    }
-
-    public OwnRecipe() {
-    }
-
-    GenericTypeIndicator<ArrayList<OwnRecipe>> t = new GenericTypeIndicator<ArrayList<OwnRecipe>>() {};
 
 }
 
